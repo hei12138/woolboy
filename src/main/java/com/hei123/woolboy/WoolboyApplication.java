da0fee9ad3ccdc48c2d853c57d10792613176cbd
李慎材 <Yealink@2019>
2019-08-01 19:28:36 +0800

    初始化项目

---
+++ src/main/java/com/hei123/woolboy/WoolboyApplication.java
@@ -0,0 +1,13 @@
+package com.hei123.woolboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoolboyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoolboyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hei123/woolboy/WoolboyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hei123.woolboy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WoolboyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
